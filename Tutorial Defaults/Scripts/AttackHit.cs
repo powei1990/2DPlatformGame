c9c9d9438983b2e46f5ae2fda8a1688bdeca88ca
Powei <powei1990@gmail.com>
2019-09-19 22:39:41 +0800

    add 2d-extras packages
    用來做rule tiles的

---
+++ Tutorial Defaults/Scripts/AttackHit.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackHit : MonoBehaviour
+{
+
+    [SerializeField] enum AttacksWhat { Enemy, Player };
+    [SerializeField] AttacksWhat attacksWhat;
+    private int launchDirection = 1;
+    [SerializeField] private GameObject parent;
+
+    //private BoxCollider2D boxCollider2d;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //boxCollider2d = GetComponent<BoxCollider2D>();
+        //ContactFilter2D contactFilter2D=new
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (boxCollider2d.enabled)
+        //{
+        //    Collider2D[] enemiesToDamage = Physics2D.OverlapCollider(boxCollider2d,LayerMask.NameToLayer("Enemy"),);
+        //}
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        Debug.Log("OnTriggerEnter");
+        //    if (col.gameObject.GetComponent(attacksWhat.ToString()) != null)
+        //    {
+        //        if (parent.transform.position.x < col.transform.position.x)
+        //        {
+        //            launchDirection = 1;
+        //        }
+        //        else
+        //        {
+        //            launchDirection = -1;
+        //        }
+        col.gameObject.GetComponent(attacksWhat.ToString()).SendMessage("Hit");
+        //    }
+
+    }
+}
