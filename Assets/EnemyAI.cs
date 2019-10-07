8b3ae15667299c7190895b671ec22714d2231e24
Powei <powei1990@gmail.com>
2019-10-07 23:10:50 +0800

    怪物AI巡邏功能
    只能針對巡邏點巡邏待改
    加入A*開源碼備用

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public enum State
+    {
+        PATROL,
+        CHASE,
+    }
+    Animator anim;
+
+    public State state;
+    private bool alive;
+    public GameObject Player;
+    public GameObject[] waypoints;
+    private int waypointInd;
+
+    public float sightDist = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        //waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        //waypointInd = Random.Range(0, waypoints.Length);
+
+        state = EnemyAI.State.PATROL;
+
+        alive = true;
+
+        StartCoroutine("FSM");
+
+    }
+    IEnumerator FSM()
+    {
+        while (alive)
+        {
+            switch(state)
+            {
+                case State.PATROL:
+                    Patrol();
+                    break;
+                case State.CHASE:
+                    Chase();
+                    break;
+
+
+            }
+        yield return null;
+
+        }
+    }
+    void Patrol()
+    {
+
+    }
+
+    void Chase()
+    {
+
+    }
+
+    public GameObject GetObject()
+    {
+        return Player;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Distance", Vector2.Distance(transform.position, Player.transform.position));
+    }
+}
---
+++ Assets/Skull_Patrol.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skull_Patrol : StateMachineBehaviour
+{
+    GameObject NPC;
+    GameObject[] waypoints;
+    int randomSpot;
+    float speed=1;
+    float waitTime;
+    float starWaitTime=3;
+
+
+    //private void Awake()
+    //{
+
+    //}
+
+    
+    // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        NPC = animator.gameObject;
+        //randomSpot = 0;
+        waypoints = GameObject.FindGameObjectsWithTag("WayPoint");
+        randomSpot = Random.Range(0, waypoints.Length);
+    }
+
+    // OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log(randomSpot);
+        if (waypoints.Length == 0) return;
+        //Debug.Log(Vector2.Distance(waypoints[currentWP].transform.position, NPC.transform.position));
+        if (Vector2.Distance(waypoints[randomSpot].transform.position,
+                            NPC.transform.position)<3.0f)
+        {
+            if (waitTime<=0)
+            {
+                randomSpot = Random.Range(0, waypoints.Length);
+                waitTime = starWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+        //Vector2 direction = (waypoints[currentWP].transform.position - NPC.transform.position);
+        NPC.transform.position = Vector2.MoveTowards(NPC.transform.position, waypoints[randomSpot].transform.position, speed * Time.deltaTime);
+        //Debug.Log();
+    }
+
+    // OnStateExit is called before OnStateExit is called on any state inside this state machine
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called before OnStateMove is called on any state inside this state machine
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateIK is called before OnStateIK is called on any state inside this state machine
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMachineEnter is called when entering a state machine via its Entry Node
+    //override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+    //{
+    //    
+    //}
+
+    // OnStateMachineExit is called when exiting a state machine via its Exit Node
+    //override public void OnStateMachineExit(Animator animator, int stateMachinePathHash)
+    //{
+    //    
+    //}
+}
